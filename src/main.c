34ff4ec84bd02b418ac0887d397867bb3dd35b21
LuichiTW <ltitaewatanabe@frba.utn.edu.ar>
2024-10-12 15:06:58 -0300

    toma archivos de toda la carpeta

--- src/main.c
+++ src/main.c
@@ -9,53 +9,84 @@
 
 int main()
 {
-  enum token t;
-  while ((t = yylex()) != FDT)
-  {
-    switch (t)
-    {
-    case INHERITS:
-      char *token = strtok(yytext, " ");
-      token = strtok(NULL, " ");
-      printf("%s ", token);
+  DIR *dir;
+  struct dirent *ent;
 
-      printf("--|> ");
-      if ((t = yylex()) == NOMBRE_VAR)
-      {
-        printf("%s\n", yytext);
-        printf("class %s{\n", token);
+
+  dir = opendir(".");
+  if(dir == NULL){
+    perror("error al abrir carpeta");
+    return 1;
+  }
+
+  enum token t;
+
+  printf("@startuml salida\n");
+
+  while ((ent = readdir(dir)) != NULL) {
+    //verificar si es un archivo .wlk
+    if (ent->d_type == DT_REG){
+      int longitud = strlen(ent->d_name);
+      if (longitud >= 4 && strcmp(&ent->d_name[longitud - 4], ".wlk") == 0) {
+        char *nombre = ent->d_name;
+
+        //abre el archivo en lectura
+        if(freopen(nombre, "r", stdin) == NULL){
+          printf("error al abrir archivo");
+          return 1;
+        }
+
+        while ((t = yylex()) != FDT)
+        {
+          switch (t)
+          {
+            case INHERITS:
+              char *token = strtok(yytext, " ");
+              token = strtok(NULL, " ");
+              printf("%s ", token);
+
+              printf("--|> ");
+              if ((t = yylex()) == NOMBRE_VAR)
+              {
+                printf("%s\n", yytext);
+                printf("class %s{\n", token);
+              }
+              break;
+            case CLASS:
+              if ((t = yylex()) == NOMBRE_VAR)
+              {
+                printf("class %s{\n", yytext);
+              }
+              break;
+            case OBJECT:
+              if ((t = yylex()) == NOMBRE_VAR)
+              {
+                printf("object %s{\n", yytext);
+              }
+              break;
+            case VAR:
+              if ((t = yylex()) == NOMBRE_VAR)
+              {
+                char *token = strtok(yytext, "=");
+                printf("-%s\n", token);
+              }
+              break;
+            case METHOD:
+              if ((t = yylex()) == NOMBRE_METHOD)
+              {
+                char *token = strtok(yytext, "=");
+                token = strtok(token, "{");
+                printf("+%s\n", token);
+              }
+              break;
+            default:
+              break;
+          }
+        }
+
       }
-      break;
-    case CLASS:
-      if ((t = yylex()) == NOMBRE_VAR)
-      {
-        printf("class %s{\n", yytext);
-      }
-      break;
-    case OBJECT:
-      if ((t = yylex()) == NOMBRE_VAR)
-      {
-        printf("object %s{\n", yytext);
-      }
-      break;
-    case VAR:
-      if ((t = yylex()) == NOMBRE_VAR)
-      {
-        char *token = strtok(yytext, "=");
-        printf("-%s\n", token);
-      }
-      break;
-    case METHOD:
-      if ((t = yylex()) == NOMBRE_METHOD)
-      {
-        char *token = strtok(yytext, "=");
-        token = strtok(token, "{");
-        printf("+%s\n", token);
-      }
-      break;
-    default:
-      break;
     }
   }
+  printf("@enduml \n");
   return 0;
 }
