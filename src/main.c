fffb66c58aac9667e0beb2731606c236a5e3af3a
LuichiTW <ltitaewatanabe@frba.utn.edu.ar>
2024-10-11 22:16:37 -0300

    scanner casi funcional

--- src/main.c
+++ src/main.c
@@ -4,36 +4,59 @@
 #include <dirent.h>    
 
 #include "tokens.h"
+#include "scanner.h"
 // scanner.l
 
-int main(){
+// ! bug en el override method no lo toma
+
+int main()
+{
   enum token t;
-  while ( (t = yylex()) != FDT)
+  while ((t = yylex()) != FDT)
   {
     switch (t)
     {
-      case INHERITS:
-        if ((t = yylex()) == NOMBRE) {
-        }
-        break;
-      case CLASS:
-        if ((t = yylex()) == NOMBRE) {
-        }
-        break;
-      case OBJECT:
-        if ((t = yylex()) == NOMBRE) {
-        }
-        break;
-      case VAR:
-        if ((t = yylex()) == NOMBRE) {
-        }
-        break;
-      case METHOD:
-        if ((t = yylex()) == NOMBRE) {
-        }
-        break;
-      default:
-        break;
+    case INHERITS:
+      char *token = strtok(yytext, " ");
+      token = strtok(NULL, " ");
+      printf("%s ", token);
+
+      printf("--|> ");
+      if ((t = yylex()) == NOMBRE_VAR)
+      {
+        printf("%s\n", yytext);
+        printf("class %s{\n", token);
+      }
+      break;
+    case CLASS:
+      if ((t = yylex()) == NOMBRE_VAR)
+      {
+        printf("class %s{\n", yytext);
+      }
+      break;
+    case OBJECT:
+      if ((t = yylex()) == NOMBRE_VAR)
+      {
+        printf("object %s{\n", yytext);
+      }
+      break;
+    case VAR:
+      if ((t = yylex()) == NOMBRE_VAR)
+      {
+        char *token = strtok(yytext, "=");
+        printf("-%s\n", token);
+      }
+      break;
+    case METHOD:
+      if ((t = yylex()) == NOMBRE_METHOD)
+      {
+        char *token = strtok(yytext, "=");
+        token = strtok(token, "{");
+        printf("+%s\n", token);
+      }
+      break;
+    default:
+      break;
     }
   }
   return 0;
